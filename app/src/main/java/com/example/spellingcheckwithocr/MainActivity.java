f6832c218d7224fa64bc6f72f9db1e556001aa93
sun2002lsw <44559110+sun2002lsw@users.noreply.github.com>
2022-10-29 03:52:16 +0900

    three tab design

--- app/src/main/java/com/example/spellingcheckwithocr/MainActivity.java
+++ app/src/main/java/com/example/spellingcheckwithocr/MainActivity.java
@@ -2,22 +2,61 @@ package com.example.spellingcheckwithocr;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.view.Menu;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
 
     Toolbar mToolbar;
+    TabLayout tabLayout;
+    ViewPager2 viewPager;
+    ViewPagerAdapter viewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 내가 만든 툴바를 가져와서 해당 툴바를 액션바로 활용
-        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        // 내가 만든 툴바를 가져와서 해당 툴바를 액션바로 설정
+        mToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
+
+        // 상단 탭
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+        viewPagerAdapter = new ViewPagerAdapter(this);
+        viewPager.setAdapter(viewPagerAdapter);
+        
+        // 탭을 직접 클릭 했을 때
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        // 탭을 스와이프 했을 때
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                tabLayout.getTabAt(position).select();
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/example/spellingcheckwithocr/ViewPagerAdapter.java
@@ -0,0 +1,35 @@
+package com.example.spellingcheckwithocr;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import fragments.checkSpelling;
+import fragments.extractString;
+import fragments.takePhoto;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+
+    public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 1:
+                return new extractString();
+            case 2:
+                return new checkSpelling();
+            default:
+                return new takePhoto();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
