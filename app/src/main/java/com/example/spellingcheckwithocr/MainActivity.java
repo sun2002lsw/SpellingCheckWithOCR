a0947bb0f0fbe4a8023cff3a91b33fd79e6ade90
sun2002lsw <44559110+sun2002lsw@users.noreply.github.com>
2022-10-29 02:35:41 +0900

    top menu design

--- app/src/main/java/com/example/spellingcheckwithocr/MainActivity.java
+++ app/src/main/java/com/example/spellingcheckwithocr/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.spellingcheckwithocr;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 내가 만든 툴바를 가져와서 해당 툴바를 액션바로 활용
+        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 }
