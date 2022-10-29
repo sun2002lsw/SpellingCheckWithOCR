c2fba71e7e00b51ea44dbd405785722f8a6d0e48
sun2002lsw <44559110+sun2002lsw@users.noreply.github.com>
2022-10-30 01:29:24 +0900

    fix grant ok bug

--- app/src/main/java/fragments/takePhoto.java
+++ app/src/main/java/fragments/takePhoto.java
@@ -24,6 +24,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.spellingcheckwithocr.MainActivity;
 import com.example.spellingcheckwithocr.R;
@@ -35,27 +36,59 @@ public class takePhoto extends Fragment {
     private ImageView imageView;
     private Button takePhotoBtn, extractStringBnt;
 
+    // 카메라 사용 허가에 대한 처리. 허가하면 그대로 이어서 카메라 실행
+    private ActivityResultLauncher<String> requestPermissionLauncher = registerForActivityResult(
+            new ActivityResultContracts.RequestPermission(),
+            new ActivityResultCallback<Boolean>() {
+                @Override
+                public void onActivityResult(Boolean result) {
+                    if (result) {
+                        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        takePictureLauncher.launch(intent);
+                    } else {
+                        Toast.makeText(getContext(), "카메라 사용을 허가 해주세요~", Toast.LENGTH_LONG);
+                    }
+                }
+            });
+
+    // 카메라 호출 후 결과 값을 어떻게 처리할 지. 이미지 뷰에 출력및 버튼 속성 변경
+    private ActivityResultLauncher<Intent> takePictureLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @SuppressLint("ResourceAsColor")
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                        Bundle extras = result.getData().getExtras();
+                        Bitmap picture = (Bitmap) extras.get("data");
+                        ((MainActivity)getActivity()).SetPicture(picture);
+
+                        AfterTakePicture(picture);
+                    }
+                }
+            });
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_take_photo, container, false);
 
         imageView = view.findViewById(R.id.picture);
 
-        // 사진 찍기 버튼. 누르면 카메라 권한 확인및 요청 -> 카메라 호출
+        // 사진 찍기 버튼. 카메라 권한이 없으면 권한 요청 & 카메라 촬영, 아니면 그냥 바로 촬영
         takePhotoBtn = view.findViewById(R.id.takePhoto);
         takePhotoBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (NeedPermission()) {
-                    return;
+                if (NeedCameraPermission()) {
+                    requestPermissionLauncher.launch(Manifest.permission.CAMERA);
+                } else {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    takePictureLauncher.launch(intent);
                 }
-
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                activityResultPicture.launch(intent);
             }
         });
 
-        // 글자 읽기 버튼. 처음에는 비활성화 & 메인 화면에서 다음 탭으로 진행
+        // 글자 읽기 버튼. 처음에는 비활성화하고 AfterTakePicture로 활성화. 누르면 다음 탭으로 진행
         extractStringBnt = view.findViewById(R.id.extractString);
         extractStringBnt.setEnabled(false);
         extractStringBnt.setOnClickListener(new View.OnClickListener() {
@@ -75,32 +108,11 @@ public class takePhoto extends Fragment {
     }
 
     // 카메라 권한 확인및 요청
-    private Boolean NeedPermission() {
-        if (ContextCompat.checkSelfPermission(this.getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this.getActivity(), new String[] {Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
-            return true;
-        }
-
-        return false;
+    private Boolean NeedCameraPermission() {
+        int cameraPermissionState = ContextCompat.checkSelfPermission(this.getContext(), Manifest.permission.CAMERA);
+        return cameraPermissionState != PackageManager.PERMISSION_GRANTED;
     }
 
-    // 카메라 호출 후 결과 값을 어떻게 처리할 지. 이미지 뷰에 출력및 버튼 속성 변경
-    ActivityResultLauncher<Intent> activityResultPicture = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
-                @SuppressLint("ResourceAsColor")
-                @Override
-                public void onActivityResult(ActivityResult result) {
-                    if (result.getResultCode() == RESULT_OK && result.getData() != null) {
-                        Bundle extras = result.getData().getExtras();
-                        Bitmap picture = (Bitmap) extras.get("data");
-                        ((MainActivity)getActivity()).SetPicture(picture);
-
-                        AfterTakePicture(picture);
-                    }
-                }
-            });
-    
     // 사진이 있을 때 후처리
     private void AfterTakePicture(Bitmap picture) {
         // 촬영 이미지
