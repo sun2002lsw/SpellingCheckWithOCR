d110ea7080c78cb8afcbb14064f2a6b2d6273a8b
sun2002lsw <44559110+sun2002lsw@users.noreply.github.com>
2022-11-05 17:24:01 +0900

    delete web view at third tab

--- app/src/main/java/fragments/checkSpelling.java
+++ app/src/main/java/fragments/checkSpelling.java
@@ -20,29 +20,6 @@ public class checkSpelling extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_check_spelling, container, false);
 
-        WebView webView = view.findViewById(R.id.webView);
-
-        webView.setWebViewClient(new WebViewClient());  // 새 창 띄우기 않기
-        webView.setWebChromeClient(new WebChromeClient());
-
-        webView.getSettings().setLoadWithOverviewMode(true);  // WebView 화면크기에 맞추도록 설정 - setUseWideViewPort 와 같이 써야함
-        webView.getSettings().setUseWideViewPort(true);  // wide viewport 설정 - setLoadWithOverviewMode 와 같이 써야함
-
-        webView.getSettings().setSupportZoom(false);  // 줌 설정 여부
-        webView.getSettings().setBuiltInZoomControls(false);  // 줌 확대/축소 버튼 여부
-
-        webView.getSettings().setJavaScriptEnabled(true); // 자바스크립트 사용여부
-//        webview.addJavascriptInterface(new AndroidBridge(), "android");
-        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true); // javascript가 window.open()을 사용할 수 있도록 설정
-        webView.getSettings().setSupportMultipleWindows(true); // 멀티 윈도우 사용 여부
-
-        webView.getSettings().setDomStorageEnabled(true);  // 로컬 스토리지 (localStorage) 사용여부
-
-
-        //웹페이지 호출
-//        webView.loadUrl("http://www.naver.com");
-        webView.loadUrl("https://www.saramin.co.kr/zf_user/tools/character-counter");
-
         return view;
     }
 }
